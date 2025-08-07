5f42509347bbe76a32cec29210035f0adad8e667
Andri <andrisomantri1324@gmail.com>
2025-08-07 09:26:18 +0700

    MIGRATION V2

--- database/migrations/2025_08_07_013722_create_staff_table.php
+++ database/migrations/2025_08_07_013722_create_staff_table.php
@@ -4,25 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('user_id');
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id('staff_id');
             $table->string('name', 100);
             $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 255);
             $table->rememberToken();
-            $table->string('phone_number', 20)->nullable();
-            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('staff');
     }
 };
