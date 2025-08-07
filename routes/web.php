25a587ede035250c1eae4b4180a6b1cef3c44cea
fransiskaa07 <martinfransis07@gmail.com>
2025-08-07 14:23:21 +0700

    Perubahan

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'OKGREEN API']);
 });
