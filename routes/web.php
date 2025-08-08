8377f925cbf03c564119feb239a11beb96113d80
sifanragn <sifanuragnia@gmail.com>
2025-08-08 11:17:44 +0700

    scroll faq,register + css

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('LandingPage');
 });
+
+Route::get('/login', function () {
+    return 'Halaman login belum dibuat';
+})->name('login');
+
+Route::get('/register', function () {
+    return view('RegisterPage');
+})->name('register');
