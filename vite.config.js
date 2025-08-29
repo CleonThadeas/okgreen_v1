afa1a070ab6fe1fc76853e9ee5cf3ea135141442
sifanragn <sifanuragnia@gmail.com>
2025-08-29 07:57:17 +0700

    add profil, rapihkan coding, sidebar add profil page, rapihkan coding, dan sidebar

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-<<<<<<< HEAD
-=======
-import tailwindcss from '@tailwindcss/vite';
->>>>>>> 7e0e9ec (tampilan Landing page header,hero, tentang kami, faq)
 
 export default defineConfig({
     plugins: [
@@ -11,9 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-<<<<<<< HEAD
-=======
-        tailwindcss(),
->>>>>>> 7e0e9ec (tampilan Landing page header,hero, tentang kami, faq)
     ],
 });
